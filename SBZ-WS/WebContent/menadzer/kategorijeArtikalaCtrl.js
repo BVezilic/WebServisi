c1c7f4241beb8f2ee1daf5932516e9879bec3712
BVezilic <banex93@gmail.com>
2016-06-25 20:24:49 +0200

    eh dokle vise

--- SBZ-WS/WebContent/menadzer/kategorijeArtikalaCtrl.js
+++ SBZ-WS/WebContent/menadzer/kategorijeArtikalaCtrl.js
@@ -1,6 +1,6 @@
 (function(angular) {
 	var app = angular.module('app');
-	app.controller('kategorijeArtikalaCtrl', ['$scope', '$http', function($scope, $http) {
+	app.controller('kategorijeArtikalaCtrl', ['$scope', '$http', 'validator', function($scope, $http, validator) {
 		var getKategorijeArtikala = function() {
 			$http({
 			  method: 'GET',
@@ -21,6 +21,10 @@
 		};
 		
 		$scope.dodajKategorijuArtikla = function() {
+			if(!validator.inRangeProcenti($scope.dodavanjPopust)) {
+				alert("Procenti moraju biti u opsego od 1-100");
+				return;
+			}
 			var kategorija = {};
 			kategorija.sifraKategorije = $scope.dodavanjeSifra;
 			kategorija.nadkategorija = $scope.dodavanjeNadkategorija;
@@ -32,12 +36,17 @@
 			  data: kategorija
 			}).then(function successCallback(response) {
 				getKategorijeArtikala();
+				resetAddFields();
 			  }, function errorCallback(response) {
 				  console.log("Greska kog ADD kategorijeArtikla");
 			  });
 		}
 		
 		$scope.azurirajKategorijuArtikla = function(kolicina) {
+			if(!validator.inRangeProcenti($scope.azuriranjePopust)) {
+				alert("Procenti moraju biti u opsego od 1-100");
+				return;
+			}
 			var azuriranaKategorija = $scope.selectedKategorijuArtikla;
 			azuriranaKategorija.nadkategorija = $scope.azuriranjeNadkategorija;
 			azuriranaKategorija.naziv = $scope.azuriranjeNaziv;
@@ -48,9 +57,23 @@
 			  data: azuriranaKategorija
 			}).then(function successCallback(response) {
 				getKategorijeArtikala();
+				resetUpdateFields();
 			  }, function errorCallback(response) {
 				  console.log("Greska kog UPDATE kategorijeKupaca");
 			  });
+		};
+		
+		var resetUpdateFields = function() {
+			$scope.azuriranjeNadkategorija = null;
+			$scope.azuriranjeNaziv = null;
+			$scope.azuriranjePopust = null;
+		};
+		
+		var resetAddFields = function() {
+			$scope.dodavanjeSifra = null;
+			$scope.dodavanjeNaziv = null;
+			$scope.dodavanjeNadkategorija = null;
+			$scope.dodavanjPopust = null;
 		};
 	}]);
 })(angular);
--- SBZ-WS/WebContent/validatorService.js
+++ SBZ-WS/WebContent/validatorService.js
@@ -1,8 +1,11 @@
 (function(angular) {
 	var app = angular.module('app');
 	app.service('validator', function() {
-	    this.inRange = function(one, two) {
-	        return two>one;
+	    this.inRange = function(first, second) {
+	        return second>first;
+	    };
+	    this.inRangeProcenti = function(procenat) {
+	    	return (procenat >= 0 && procenat <= 100);
 	    };
 	});
 })(angular);
